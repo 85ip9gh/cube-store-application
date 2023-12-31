4a10fd72c6e5ad226a6cf3c146adc7d90ad19787
Pesanth Janaseth Rangaswamy Anitha <119688837+85ip9gh@users.noreply.github.com>
2023-12-30 22:16:28 -0400

    Implement Functionality to Cart Icon
    let items in cart icon be dynamically added and be removed when clicking remove cart button

--- web-store/src/app/components/header/header.component.ts
+++ web-store/src/app/components/header/header.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  
-  constructor() {
+  private _cart: Cart = { items: [] };
+  itemsQuantity: number = 0;
+
+  @Input()
+  get cart(): Cart {
+    return this._cart;
   }
 
-  ngOnInit(): void {
+  set cart(cart: Cart) {
+    this._cart = cart;
+    this.itemsQuantity = cart.items
+    .map(item => item.quantity)
+    .reduce((prev, curr) => prev + curr, 0);
   }
+
+  constructor(private cartService: CartService) {
+  }
+
+  getTotal(items: CartItem[]): number {
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
 }
