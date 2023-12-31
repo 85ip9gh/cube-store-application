08220d04043949b15ee0a1f2b67132cd2f3f3406
Pesanth Janaseth Rangaswamy Anitha <pesanth10@gmail.com>
2023-12-30 21:43:56 -0400

    Add Message and CartService Implementation
    Let snackbar message pop up once item is added to cart

--- web-store/src/app/pages/home/components/product-box/product-box.component.ts
+++ web-store/src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -9,4 +10,18 @@ import { Component, Input } from '@angular/core';
 export class ProductBoxComponent {
   @Input() fullWidthMode:boolean = false;
 
+  product: Product | undefined ={
+    id: 1,
+    title: 'Zirconium Bastion',
+    price: 120,
+    category: 'Metal',
+    description: 'In the mystical realm of Eldoria, nestled within the ethereal forests of Lumina Glades, there lies a hidden sanctuary known as the Zirconium Bastion. Legend speaks of a magnificent metal cube, forged from the rarest and purest Zirconium, that serves as both protector and keeper of ancient secrets and treasures. The Bastion is guarded by the mighty Zirconium Golem, a creature of immense power and strength, who will stop at nothing to protect the Bastion from those who seek to steal its secrets.',
+    image: 'https://via.placeholder.com/150'
+  };
+
+  @Output() addToCart = new EventEmitter();
+
+  onAddToCart():void {
+    this.addToCart.emit(this.product);
+  }
 }
