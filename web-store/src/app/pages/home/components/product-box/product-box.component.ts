566113263122ca6cd7ebbf68037aae5001965cab
Pesanth Janaseth Rangaswamy Anitha <119688837+85ip9gh@users.noreply.github.com>
2023-12-31 20:14:53 -0400

    Implement Store Service to Get Products from fakestoreapi.com
    connect the results of api get request to home component and product box

--- web-store/src/app/pages/home/components/product-box/product-box.component.ts
+++ web-store/src/app/pages/home/components/product-box/product-box.component.ts
@@ -10,14 +10,7 @@ import { Product } from 'src/app/models/product.model';
 export class ProductBoxComponent {
   @Input() fullWidthMode:boolean = false;
 
-  product: Product | undefined ={
-    id: 1,
-    title: 'Zirconium Bastion',
-    price: 120,
-    category: 'Metal',
-    description: 'In the mystical realm of Eldoria, nestled within the ethereal forests of Lumina Glades, there lies a hidden sanctuary known as the Zirconium Bastion. Legend speaks of a magnificent metal cube, forged from the rarest and purest Zirconium, that serves as both protector and keeper of ancient secrets and treasures. The Bastion is guarded by the mighty Zirconium Golem, a creature of immense power and strength, who will stop at nothing to protect the Bastion from those who seek to steal its secrets.',
-    image: 'https://via.placeholder.com/150'
-  };
+  @Input() product: Product | undefined;
 
   @Output() addToCart = new EventEmitter();
 
---
+++ web-store/src/app/services/store.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product.model';
+import { Observable } from 'rxjs';
+
+const STORE_BASE_URL = 'http://fakestoreapi.com';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService {
+
+  constructor(private httpClient: HttpClient) {}
+
+  getAllProducts(limit = '12', sort='desc'): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(
+      `${STORE_BASE_URL}/products?sort=${sort}&limit=${limit}`
+      );
+  }
+}
