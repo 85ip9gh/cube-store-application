2e6b77dbb3313a980db1b158a885d7597b605b62
Pesanth Janaseth Rangaswamy Anitha <pesanth10@gmail.com>
2024-02-04 08:53:05 -0400

    add price fikter to frontend
    minimum and maximum price value sort for cubes

--- web-store/src/app/app.module.ts
+++ web-store/src/app/app.module.ts
@@ -26,6 +26,8 @@ import { CartComponent } from './pages/cart/cart.component';
 import { CartService } from './services/cart.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './components/footer/footer.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
@@ -55,6 +57,8 @@ import { FooterComponent } from './components/footer/footer.component';
     MatTableModule,
     MatBadgeModule,
     MatSnackBarModule,
+    MatInputModule,
+    MatFormFieldModule,
     HttpClientModule
   ],
   providers: [CartService],
