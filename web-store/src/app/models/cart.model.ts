cad4ef4779b3fcdcde814e73bedadbafbafb8a0d
Pesanth Janaseth Rangaswamy Anitha <119688837+85ip9gh@users.noreply.github.com>
2024-02-06 06:57:35 -0400

    Add some comments to code
    header, footer, and models

--- web-store/src/app/models/cart.model.ts
+++ web-store/src/app/models/cart.model.ts
@@ -1,7 +1,10 @@
+// Interface for the cart space: holds cart items
 export interface Cart {
     items: Array<CartItem>;
 }
 
+
+// defining the cart item
 export interface CartItem {
     product: string;
     name: string;
