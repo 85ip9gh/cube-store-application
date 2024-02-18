3c283be8543f5a81bde915a74ed8d0e98ce2f7f1
Pesanth Janaseth Rangaswamy Anitha <pesanth10@gmail.com>
2024-02-17 22:40:53 -0400

    Make website more mobile-friendly
    change drawerService states to BehaviorSubject, update cart page for mobile, change some stuff in home page for mobile

--- web-store/src/app/pages/cart/cart.component.ts
+++ web-store/src/app/pages/cart/cart.component.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { loadStripe } from '@stripe/stripe-js';
+import { Subscription } from 'rxjs';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
+import { DrawerService } from 'src/app/services/drawer.service';
 
 // for AWS EC2 instance
 const STORE_BASE_URL = 'http://18.118.238.40:4242';
@@ -19,18 +21,35 @@ const STORE_BASE_URL = 'http://18.118.238.40:4242';
 export class CartComponent {
   
   cart: Cart = {items: []}
+  mobile: boolean = false;
+
+  private mobileStateSubscription: Subscription;
 
   dataSource: CartItem[] = [];
   displayedColumns: string[] = ['product', 'name', 'quantity', 'price', 'total', 'action'];
 
-    constructor(private cartService: CartService, private http: HttpClient) { }
+    constructor(private cartService: CartService, private drawerService: DrawerService, private http: HttpClient) { 
+      this.mobileStateSubscription = new Subscription();
+    }
 
     ngOnInit(): void {
       this.dataSource = this.cart.items;
+      this.drawerService.toggleCart(true);
       this.cartService.cart.subscribe((_cart: Cart) => {
         this.cart = _cart;
         this.dataSource = _cart.items;
       });
+      this.mobileStateSubscription = this.drawerService.mobileState$.subscribe(mobile => {
+        this.mobile = mobile;
+        if (this.mobile) {
+          this.displayedColumns = ['product', 'quantity', 'total'];
+        } else {
+          this.displayedColumns = ['product', 'name', 'quantity', 'price', 'total', 'action'];
+        }
+        console.log(this.displayedColumns);
+        console.log("cart component mobile value: " + this.mobile);  
+      });
+      
     }
 
     getTotal(items: CartItem[]): number {
@@ -53,6 +72,10 @@ export class CartComponent {
       this.cartService.subtractQuantity(item);
     }
 
+    updateCartPage(): void {
+      this.drawerService.toggleCart(false);
+    }
+
     onCheckout(): void {
       this.http.post( `${STORE_BASE_URL}/checkout`, {
         items: this.cart.items
@@ -60,5 +83,10 @@ export class CartComponent {
         let stripe = await loadStripe('pk_test_51OTZqzA7JcW8doruYawTDrUzXPGQ8mQaqf0i7QwmhveJskGH6U991v0MwWHBBor2xUiagg86owYKlnDwwp6QZ5tx009eEEEJyK');
         stripe?.redirectToCheckout({ sessionId: res.id });
       });
+    }
+
+    ngOnDestroy(): void {
+      // Unsubscribe from the subscription to prevent memory leaks
+      this.mobileStateSubscription.unsubscribe();
     }
 }
