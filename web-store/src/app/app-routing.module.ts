133554683f56c360902a595c6a6cbd67f3841ef6
Pesanth Janaseth Rangaswamy Anitha <119688837+85ip9gh@users.noreply.github.com>
2024-02-18 08:45:19 -0400

    implement Lazy Loading
    Lazily load modules and images. Use lazy load image directive.

--- web-store/src/app/app-routing.module.ts
+++ web-store/src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { CartComponent } from './pages/cart/cart.component';
 
-const routes: Routes = [{
-  path: 'home',
-  component: HomeComponent
-},{
-  path: 'cart', 
-  component: CartComponent
-},
-{
-  path: '', redirectTo: '/home', pathMatch: 'full'
-}];
+const routes: Routes = [
 
+  { 
+    path: 'home', 
+    loadChildren: () => import('./pages/home/components/modules/home/home.module').then(m => m.HomeModule) 
+  },
+  { 
+    path: 'cart', 
+    loadChildren: () => import('./pages/home/components/modules/cart/cart.module').then(m => m.CartModule) 
+  },
+  {
+    path: '', redirectTo: '/home', pathMatch: 'full'
+  }];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
