786a47c0a0aedfa7fc54cda6ad48f03f69d1d5df
Pesanth Janaseth Rangaswamy Anitha <pesanth10@gmail.com>
2024-02-18 08:45:19 -0400

    implement Lazy Loading
    Lazily load modules and images. Use lazy load image directive.

--- web-store/src/app/pages/home/components/modules/cart/cart.module.ts
+++ web-store/src/app/pages/home/components/modules/cart/cart.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule, FormsModule  } from '@angular/forms';
+import { CartRoutingModule } from './cart-routing.module';
+import { CartComponent } from './cart.component';
+
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -18,31 +17,20 @@ import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatTableModule} from '@angular/material/table';
 import { MatBadgeModule} from '@angular/material/badge';
 import { MatSnackBarModule} from '@angular/material/snack-bar';
-import { HeaderComponent } from './components/header/header.component';
-import { CartService } from './services/cart.service';
 import { HttpClientModule } from '@angular/common/http';
-import { FooterComponent } from './components/footer/footer.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
+    CartComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSidenavModule,
-    MatGridListModule,
-    MatMenuModule,
-    MatButtonModule,
+    CommonModule,
+    CartRoutingModule,
     MatCardModule,
     MatIconModule,
-    MatExpansionModule,
     MatListModule,
     MatToolbarModule,
     MatTableModule,
@@ -51,10 +39,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatInputModule,
     MatFormFieldModule,
     HttpClientModule,
-    ReactiveFormsModule,
-    FormsModule
-  ],
-  providers: [CartService],
-  bootstrap: [AppComponent]
+    MatButtonModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatExpansionModule
+  ]
 })
-export class AppModule { }
+export class CartModule { }
