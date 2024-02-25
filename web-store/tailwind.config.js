64d9eabb3d65feea05192f59f07057b4cc4d1f08
Pesanth Janaseth Rangaswamy Anitha <pesanth10@gmail.com>
2024-02-25 13:40:27 -0400

    Change Angular icon to cube
    replace favicon.ico with cube.png in angular.json

--- web-store/tailwind.config.js
+++ web-store/tailwind.config.js
@@ -1,10 +1,27 @@
+/* 
+--text: #061c09;
+--background: #f1fcf2;
+--primary: #3bd748;
+--secondary: #84dde6;
+--accent: #65ace0; 
+*/
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: '#061c09',
+        background: '#f1fcf2',
+        primary: '#3bd748',
+        secondary: '#84dde6',
+        accent: '#65ace0',
+      },
+    },
   },
   plugins: [],
 }
