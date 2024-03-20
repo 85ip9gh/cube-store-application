8a789c1b2e9fc10a6813e37812e2acbd9745dcba
Pesanth Janaseth Rangaswamy Anitha <pesanth10@gmail.com>
2024-03-19 22:56:45 -0300

    Upload cubes to mongodb
    successfully upload 46 cubes to mongodb through post request

---
+++ web-store/server/routes/cube.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  getAllCubes,
+  getSortedCubes,
+  getCubeCategories,
+  getCubeSizes,
+  addCubes,
+  deleteAllCubes,
+} from "../controllers/cube.controller.js";
+
+const cubeRouter = express.Router();
+
+cubeRouter.get("/cubes", getAllCubes);
+cubeRouter.get('/cubes(/category/:category)?', getSortedCubes);
+cubeRouter.get('/cubes/categories', getCubeCategories);
+cubeRouter.get('/cubes/sizes', getCubeSizes);
+cubeRouter.post('/cubes', addCubes);
+cubeRouter.delete('/cubes', deleteAllCubes);
+
+export default cubeRouter;
