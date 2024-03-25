3d1db5ea694019f060e87e0867b448c5cff55742
Pesanth Janaseth Rangaswamy Anitha <pesanth10@gmail.com>
2024-03-25 10:48:05 -0300

    Update footer
    Add color to github and LinkedIn links

--- web-store/src/app/pages/components/modules/login/login.module.ts
+++ web-store/src/app/pages/components/modules/login/login.module.ts
@@ -7,6 +7,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -20,7 +22,9 @@ import { MatButtonModule } from '@angular/material/button';
   MatInputModule,
   MatIconModule,
   MatButtonModule,
-  MatFormFieldModule
+  MatFormFieldModule,
+  FormsModule,
+  RouterModule,
 ]
 })
 export class LoginModule { }
